964553ab1a41cb2e1d53cb084baad4f0657ae87e
Dev-J0 <dityx.obsidian@gmail.com>
2023-08-01 15:46:50 +0200

    didn't pay attention, forgot to change from app to greeting

---
+++ src/greeting.jsx
@@ -0,0 +1,7 @@
+function Greeting() {
+    return <h2>&quot; You can't CTRL-Z in RL.&quot;</h2>
+}
+
+//In the above example, &quot; is an escape code we use to render ". Your linter will greet you with an error if you use regular quotes.
+
+export default Greeting;
